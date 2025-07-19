266a4cb4eeffbe2d15674e3db65f2895ec5a96a9
bydhorizon0 <bydhorizon0@gmail.com>
2025-07-20 00:09:20 +0900

    영화 검색 페이지네이션에 필요한 모델 추가

---
+++ TheaterWebApp/Models/MovieSearchRequest.cs
@@ -0,0 +1,35 @@
+﻿namespace TheaterWebApp.Models;
+
+public class MovieSearchRequest
+{
+    private string _keyword = string.Empty;
+
+    public string Keyword
+    {
+        get => _keyword;
+        set => _keyword = value ?? string.Empty;
+    }
+
+    private string _type = string.Empty;
+
+    public string Type
+    {
+        get => _type;
+        set => _type = value ?? string.Empty;
+    }
+    
+    private int _page = 1;
+    public int Page
+    {
+        get => _page;
+        set => _page = value > 0 ? value : 1;
+    }
+
+    private int _size = 12;
+
+    public int Size
+    {
+        get => _size;
+        set => _size = value > 0 ? value : 12;
+    }
+}
---
+++ TheaterWebApp/Models/PageResult.cs
@@ -0,0 +1,11 @@
+﻿namespace TheaterWebApp.Models;
+
+public class PageResult<T>
+{
+    public ICollection<T> Items { get; set; } = new  List<T>();
+    public int TotalCount { get; set; }
+    public int TotalPages { get; set; }
+    public int CurrentPage { get; set; }
+    public string SearchKeyword { get; set; } = string.Empty;
+    public string SearchType { get; set; } = string.Empty;
+}
