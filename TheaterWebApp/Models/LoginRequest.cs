d4044c165976a48aad4b260465fdca8df5b8dcde
bydhorizon0 <bydhorizon0@gmail.com>
2025-07-20 15:46:59 +0900

    로그인 모델 추가 - LoginRequest

---
+++ TheaterWebApp/Models/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheaterWebApp.Models;
+
+public class LoginRequest
+{
+    [EmailAddress(ErrorMessage = "이메일 형식에 맞게 입력해주세요.")]
+    [Required(ErrorMessage = "이메일은 필수입니다.")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "비밀번호는 필수입니다.")]
+    public string Password { get; set; }
+}
